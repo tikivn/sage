655478907407c65983b580ce2eaf1a7626a828a2
Anh Dung Bui <dungba.sg@gmail.com>
2019-06-30 12:02:10 +0700

    add name to events

--- common/src/main/java/org/sage/common/events/SageEvent.java
+++ common/src/main/java/org/sage/common/events/SageEvent.java
@@ -3,6 +3,8 @@ package org.sage.common.events;
 import java.io.Serializable;
 
 public interface SageEvent<K extends Serializable> extends Serializable {
+    
+    public String getName();
 
     public K getKey();
 }
